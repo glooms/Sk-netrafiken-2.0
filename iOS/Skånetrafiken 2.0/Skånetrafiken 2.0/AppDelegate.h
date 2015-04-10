375a8857ce75df827c8d384b28e92f101887ed2f
Jacob Arvidsson <jacobarvidsson@me.com>
2015-04-10 16:42:02 +0200

    iOS project added.

---
+++ iOS/Skånetrafiken 2.0/Skånetrafiken 2.0/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Skånetrafiken 2.0
+//
+//  Created by Jacob Arvidsson on 2015-04-10.
+//  Copyright (c) 2015 jacobarvidsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/Skånetrafiken 2.0/Skånetrafiken 2.0/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Skånetrafiken 2.0
+//
+//  Created by Jacob Arvidsson on 2015-04-10.
+//  Copyright (c) 2015 jacobarvidsson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
