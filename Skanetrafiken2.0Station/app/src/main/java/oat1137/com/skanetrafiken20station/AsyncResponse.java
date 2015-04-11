2b1e9891ea24baa2ba118a7f73c5811fd1f17f39
Olle Tervalampi-Olsson <olleto@gmail.com>
2015-04-11 15:50:06 +0200

    MVD

---
+++ Skanetrafiken2.0Station/app/src/main/java/oat1137/com/skanetrafiken20station/AsyncResponse.java
@@ -0,0 +1,11 @@
+package oat1137.com.skanetrafiken20station;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by olle on 2015-04-11.
+ */
+interface AsyncResponse {
+    void processFinish(ArrayList<Station> stations);
+}
---
+++ Skanetrafiken2.0Station/app/src/main/java/oat1137/com/skanetrafiken20station/Departure.java
@@ -0,0 +1,18 @@
+package oat1137.com.skanetrafiken20station;
+
+/**
+ * Created by olle on 2015-04-11.
+ */
+
+
+public class Departure {
+    int id;
+    String time;
+    String dest;
+
+    public Departure(int id, String time, String dest) {
+        this.id = id;
+        this.time = time;
+        this.dest = dest;
+    }
+}
