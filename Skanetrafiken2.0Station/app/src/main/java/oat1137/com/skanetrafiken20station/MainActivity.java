b3b0ede165e51808404d8632f2eeba6847743915
Olle Tervalampi-Olsson <olleto@gmail.com>
2015-04-11 17:13:46 +0200

    Refactored ClosestStationFinder

--- Skanetrafiken2.0Station/app/src/main/java/oat1137/com/skanetrafiken20station/MainActivity.java
+++ Skanetrafiken2.0Station/app/src/main/java/oat1137/com/skanetrafiken20station/MainActivity.java
@@ -31,27 +31,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity  {
+public class MainActivity extends ActionBarActivity implements AsyncResponse {
     Station closestStation;
-    FindClosestStation fcs;
+    StationFinder sf;
     FindDepartures fd;
     public static final String TAG = "Skanetrafiken";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
         Log.d("Main", "Started application");
         Intent i = new Intent(this, SkanetrafikenService.class);
-        // SkanetrafikenService sts = new SkanetrafikenService();
-        fcs = new FindClosestStation();
-        fcs.execute();
-        while (closestStation == null) {
-            Log.d(TAG, "WAITING");
+        sf = new StationFinder();
+        sf.setListener(this);
+        sf.execute();
+    }
+    @Override
+    public void processFinish(ArrayList<Station> stations) {
+        double minDistance = Double.MAX_VALUE;
+        for (Station s : stations) {
+            if (s.getDistance() < minDistance) {
+                minDistance = s.getDistance();
+                closestStation = s;
+            }
         }
+
         fd = new FindDepartures();
         fd.execute();
+        Log.d(TAG, "CLOSEST STATION " + closestStation.getName());
+        Log.d(TAG, "CLOSEST STATIONID " + closestStation.getId());
     }
-
     public void finishReadStations(ArrayList<Station> stations){
                     /* find station with lowest distance */
         double minDistance = Double.MAX_VALUE;
@@ -94,134 +102,7 @@ public class MainActivity extends ActionBarActivity  {
 
         return super.onOptionsItemSelected(item);
     }
-    class FindClosestStation extends AsyncTask {
-
-        String getStationURL;
-        String getDeparturesURL;
-        public AsyncResponse delegate=null;
-
-        protected void onPostExecute(ArrayList<Station> stations) {
-            super.onPostExecute(stations);
-
-        }
-        @Override
-        protected Object doInBackground(Object[] params) {
-            Log.d(TAG, "INTO DOINBACKGROUND");
-            Station s;
-            ArrayList<Station> stations = null;
-            // Create a new HttpClient and Post Header
-            Log.d(TAG, "INTO postData");
-            HttpClient httpclient = new DefaultHttpClient();
-            double x = 6179310;
-            double y = 1338078;
-            double R = 50;
-            HttpGet httpget = new HttpGet("http://www.labs.skanetrafiken.se/v2.2/neareststation.asp?x=6179309&y=1338077&R=500"); //"www.labs.skanetrafiken.se/v2.2/trafficmeans.asp");
-            try {
-                // Execute HTTP Post Request
-                HttpResponse response = httpclient.execute(httpget);
-                HttpEntity resEntity = response.getEntity();
-                //Response
-                try {
-                    XmlPullParser parser = Xml.newPullParser();
-                    parser.setInput(resEntity.getContent(), null);
-                    stations = new ArrayList<>();
-
-                    String name;
-                    while (parser.next() != XmlPullParser.END_DOCUMENT) {
-                        name = parser.getName();
-                        if (parser.getEventType() != XmlPullParser.START_TAG) {
-                            continue;
-                        }
-                        /* Read stop area information */
-
-                        if (name.equals("NearestStopArea")) {
-                            Log.d(TAG, "Before readStation. Name: " + name);
-                            s = readStation(parser);
-                            if (s != null) {
-                                stations.add(s);
-                            }
-                        }
-                        Log.d(TAG, "After readStation. Name: " + parser.getName());
-                    }
-                    Log.d(TAG, "After reading xml");
-                } catch (XmlPullParserException e) {
-                    Log.d(TAG, "PullParserException: " + e.getMessage());
-                } catch (IOException e){
-                    Log.d(TAG, "IOException: " + e.getMessage());
-                }
-            } catch (ClientProtocolException e) {
-                // TODO Auto-generated catch block
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-            }
-            Log.d(TAG, "NR OF STATIONS " + stations.size());
-            finishReadStations(stations);
-            return null;
-        }
-    }
-    private Station readStation(XmlPullParser parser) throws XmlPullParserException, IOException {
-        final String TAG = "Skanetrafiken";
-        parser.next();
-        String type = parser.getName();
-        String name = "";
-        int id = -1;
-        double X = -1;
-        double Y = -1;
-        double distance = -1;
-        Log.d(TAG, "ReadParser: Type: " + type);
-        while (!parser.getName().equals("NearestStopArea")) {
-            if (type.equals("Id")) {
-                parser.next();
-                String sId = parser.getText();
-                Log.d(TAG, "ID: " + sId);
-                if (sId != null) {
-                    id = Integer.valueOf(sId);
-                }
-                parser.next();
-            } else if (type.equals("X")) {
-                parser.next();
-                String sX = parser.getText();
-                Log.d(TAG, "X: " + sX);
-                if (sX != null) {
-                    X = Integer.valueOf(sX);
-                }
-                parser.next();
-            } else if (type.equals("Y")) {
-                parser.next();
-                String sY = parser.getText();
-                Log.d(TAG, "Y: " + sY);
-                if (sY != null) {
-                    Y = Integer.valueOf(sY);
-                }
-                parser.next();
-            } else if (type.equals("Name")) {
-                parser.next();
-                String sName = parser.getText();
-                Log.d(TAG, "Name: " + sName);
-                if (sName != null) {
-                    name = sName;
-                }
-                parser.next();
-            } else if (type.equals("Distance")) {
-                parser.next();
-                String sDist = parser.getText();
-                Log.d(TAG, "Distance: " + sDist);
-                if (sDist != null) {
-                    distance = Integer.valueOf(sDist);
-                }
-                parser.next();
-            } else {
-                Log.d(TAG, "ELSE: " + parser.getText());
-            }
-            parser.next();
-            type = parser.getName();
-        }
-        if (!name.equals("") && id != -1 && X != -1 && Y != -1 && distance != -1) {
-            return new Station(id, X, Y, name, distance);
-        }
-        return null;
-    }
-    class FindDepartures extends AsyncTask {
+      class FindDepartures extends AsyncTask {
         final String TAG = "Skanetrafiken";
         protected void onPostExecute(ArrayList<Station> stations) {
             super.onPostExecute(stations);
